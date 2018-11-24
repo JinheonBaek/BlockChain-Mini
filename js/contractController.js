1ca28cf51a71d7a1809012a7916a182f927abb13
JinheonBaek <Jinheon.Baek@outlook.kr>
2018-11-24 16:40:35 +0900

    Finish

--- js/contractController.js
+++ js/contractController.js
@@ -2,6 +2,8 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('http://40.87.49.249:8123'));
 
+var account;
+
 function createAccount() {
 	var password = $("#password").val();
 	var newAccount = web3.personal.newAccount(password);
@@ -10,20 +12,23 @@ function createAccount() {
 
 // Vote for Multiple
 var Voting = web3.eth.contract([{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]);
-var votingContract = Voting.at("0xFF401002548D1c2Cadce72f2F383Ac5A78226203");
+var votingContract = Voting.at("0x4135ea09f1b8bb96307427e3a098e01fcd3be51f");
 
 // Vote for Once
 // var Voting = web3.eth.contract([{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]);
 // var votingContract = Voting.at("0x4D5cbb592993971a3a3cB8F6FBA630e60C66C70B");
 
-var candidates = {"alice": "candidate-1", "bob": "candidate-2"}
+var candidates = {"김동원": "candidate-1", "남기춘": "candidate-2", "선경": "candidate-3", "이두희": "candidate-4", "정영환": "candidate-5", "정진택": "candidate-6", "최광식": "candidate-7"};
 
 function voteForCandidate(candidate) {
-	candidateName = $("#candidate").val();
-
+	// candidateName = $("#candidate").val();
+	candidateName = candidate;
+	console.log(candidateName)
+	
 	votingContract.voteForCandidate(candidateName, {from: account, gas: 4700000}, function() {
 		let div_id = candidates[candidateName];
 		$("#" + div_id).html(votingContract.totalVotesFor.call(candidateName).toString());
+		console.log("완료");
 	});
 }
 
@@ -46,6 +51,7 @@ $(document).ready(function() {
 
 	for(var i=0; i < candidateNames.length; i++){
 		let name = candidateNames[i];
+		console.log(name)
 		let val = votingContract.totalVotesFor.call(name).toNumber();
 		$("#" + candidates[name]).html(val);
 	}
